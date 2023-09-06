027f20aaa3a362dfa5b5a0fa61e3a065737d44f5
elpachongco <earlsiachongco@gmail.com>
2023-09-06 12:40:37 +0800

    Use django-allauth views.

--- bhread/core/urls.py
+++ bhread/core/urls.py
@@ -41,17 +41,6 @@ urlpatterns = [
     path("post-children/<int:pk>/", feed_views.post_children, name="children"),
     path("post/<int:pk>/", feed_views.post_detail, name="detail"),
     path("profile/", user_views.profile, name="profile"),
-    path("register/", user_views.register, name="register"),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="user/login.html"),
-        name="login",
-    ),
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(template_name="user/logout.html"),
-        name="logout",
-    ),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
