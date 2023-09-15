d0823c5dfcbc22377be7acd150b77c4861d46ac7
elpachongco <earlsiachongco@gmail.com>
2023-09-15 16:16:05 +0800

    rename media_root to mediafiles.

--- bhread/core/settings/base.py
+++ bhread/core/settings/base.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 MEDIA_URL = "/media/"
 
 # Default primary key field type
