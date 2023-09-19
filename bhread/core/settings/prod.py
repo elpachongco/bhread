30f994bf53b80cb7b913979d724a78e349703060
elpachongco <earlsiachongco@gmail.com>
2023-09-19 10:41:48 +0800

    Use redis for caching in prod.

--- bhread/core/settings/prod.py
+++ bhread/core/settings/prod.py
@@ -24,7 +24,8 @@ DATABASES = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379",
     }
 }
 
