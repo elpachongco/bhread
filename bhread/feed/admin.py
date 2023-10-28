8e731dfe7dd36118c8c4a4fbd25a7230ca39624c
elpachongco <earlsiachongco@gmail.com>
2023-10-28 14:18:41 +0800

    Add GroupConfig to admin

--- bhread/feed/admin.py
+++ bhread/feed/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Category, Feed, Post, Vote
+from .models import Category, Feed, GroupConfig, Post, Vote
 
 admin.site.register(Feed)
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Vote)
+admin.site.register(GroupConfig)
